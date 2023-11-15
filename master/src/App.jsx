0f77c8e8e32688563ab46814de00dd284e661fb6
prudhvi2297 <mpnc2296@gmail.com>
2023-11-15 08:32:05 -0500

    Inital Project Setup

---
+++ master/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-[#32a852]">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
