182d35d479181e8ce4c23283e3053e92450d89c8
prudhvi2297 <mpnc2296@gmail.com>
2023-11-17 05:37:09 -0500

    Frontend Admin Orders Page

---
+++ master/src/views/Pagination.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
+
+const Pagination = ({
+  pageNumber,
+  setPageNumber,
+  totalItem,
+  parPage,
+  showItem,
+}) => {
+  let totalPage = Math.ceil(totalItem / parPage);
+  let startPage = pageNumber;
+
+  let dif = totalPage - pageNumber;
+
+  if (dif <= showItem) {
+    startPage = totalPage - showItem;
+  }
+  let endPage = startPage < 0 ? showItem : showItem + startPage;
+
+  if (startPage <= 0) {
+    startPage = 1;
+  }
+
+  const createBtn = () => {
+    const btns = [];
+
+    for (let i = startPage; i <= endPage; i++) {
+      btns.push(
+        <li
+          key={i}
+          onClick={() => setPageNumber(i)}
+          className={`
+            ${
+              pageNumber === i
+                ? "bg-white shadow-lg shadow-indigo-500/50 text-black"
+                : "bg-slate-700 hover:bg-black shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]"
+            } w-[30px] h-[30px] rounded-full flex justify-center items-center cursor-pointer`}
+        >
+          {i}
+        </li>
+      );
+    }
+    return btns;
+  };
+
+  return (
+    <ul className="flex gap-3">
+      {pageNumber > 1 && (
+        <li
+          onClick={() => setPageNumber(pageNumber - 1)}
+          className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-700 text-[#d0d2d6] cursor-pointer"
+        >
+          <AiOutlineDoubleLeft />
+        </li>
+      )}
+      {createBtn()}
+      {pageNumber < totalPage && (
+        <li
+          onClick={() => setPageNumber(pageNumber + 1)}
+          className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-700 text-[#d0d2d6] cursor-pointer"
+        >
+          <AiOutlineDoubleRight />
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Pagination;
