d33871d0a48c5b939b52d5acad95a9db47183669
prudhvi2297 <mpnc2296@gmail.com>
2023-11-22 04:50:20 -0500

    Backend Seller Add Product

---
+++ master/src/store/Reducers/productReducer.js
@@ -0,0 +1,69 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const add_product = createAsyncThunk(
+  'product/add_product',
+  async (product, { rejectWithValue, fulfillWithValue }) => {
+      try {
+          const { data } = await api.post('/product-add', product, { withCredentials: true })
+          return fulfillWithValue(data)
+      } catch (error) {
+          return rejectWithValue(error.response.data)
+      }
+  }
+)
+
+export const get_product = createAsyncThunk(
+  "product/get_product",
+  async (
+    { parPage, page, searchValue },
+    { rejectWithValue, fulfillWithValue }
+  ) => {
+    try {
+      const { data } = await api.get(
+        `/product-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,
+        { withCredentials: true }
+      );
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const productReducer = createSlice({
+  name: "product",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    products: [],
+    totalProducts: 0,
+  },
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = "";
+      state.successMessage = "";
+    },
+  },
+  extraReducers: {
+    [add_product.pending]: (state, _) => {
+      state.loader = true;
+    },
+    [add_product.rejected]: (state, { payload }) => {
+      state.loader = false;
+      state.errorMessage = payload.error;
+    },
+    [add_product.fulfilled]: (state, { payload }) => {
+      state.loader = false;
+      state.successMessage = payload.message;
+      state.products = [...state.products, payload.product];
+    },
+    [get_product.fulfilled]: (state, { payload }) => {
+      state.totalProducts = payload.totalProducts;
+      state.products = payload.products;
+    },
+  },
+});
+export const { messageClear } = productReducer.actions;
+export default productReducer.reducer;
