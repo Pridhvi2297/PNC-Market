233a9636fad60b22389487f899a78ed09f841d45
prudhvi2297 <mpnc2296@gmail.com>
2023-11-23 06:46:12 -0500

    Frontend and Backend Add Product

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,18 +2,20 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async (req, res, next) => {
     const { accessToken } = req.cookies;
-
-    try {
-        if (!accessToken) {
-            throw new Error('Please login to access');
+    if (!accessToken) {
+        return res.status(409).json({ error: 'Please login'})
+        
+    } else {
+        try {
+            const deCodeToken = await jwt.verify
+            (accessToken, process.env.SECRET)
+            req.role = deCodeToken.role
+            req.id = deCodeToken.id
+            next()
+        } catch (error) {
+            return res.status(409).json({error: 'Please Login'})
+            
         }
-
-        const decodedToken = await jwt.verify(accessToken, process.env.SECRET);
-        req.role = decodedToken.role;
-        req.id = decodedToken.id;
-        next();
-    } catch (error) {
-        // Centralized error handling
-        return res.status(401).json({ error: error.message });
+        
     }
 };
--- master/src/store/Reducers/categoryReducer.js
+++ master/src/store/Reducers/categoryReducer.js
@@ -1,74 +1,68 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../../api/api";
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import api from '../../api/api'
 export const categoryAdd = createAsyncThunk(
-  "category/categoryAdd",
-  async ({ name, image }, { rejectWithValue, fulfillWithValue }) => {
-    try {
-      const formData = new FormData();
-      formData.append("name", name);
-      formData.append("image", image);
-      const { data } = await api.post("/category-add", formData, {
-        withCredentials: true,
-      });
-      return fulfillWithValue(data);
-    } catch (error) {
-      return rejectWithValue(error.response.data);
+    'category/categoryAdd',
+    async ({ name, image }, { rejectWithValue, fulfillWithValue }) => {
+        try {
+            const formData = new FormData()
+            formData.append('name', name)
+            formData.append('image', image)
+            const { data } = await api.post('/category-add', formData, { withCredentials: true })
+            return fulfillWithValue(data)
+        } catch (error) {
+            return rejectWithValue(error.response.data)
+        }
     }
-  }
-);
+)
 
 export const get_category = createAsyncThunk(
-  "category/get_category",
-  async (
-    { parPage, page, searchValue },
-    { rejectWithValue, fulfillWithValue }
-  ) => {
-    try {
-      const { data } = await api.get(
-        `/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,
-        { withCredentials: true }
-      );
-      return fulfillWithValue(data);
-    } catch (error) {
-      return rejectWithValue(error.response.data);
+    'category/get_category',
+    async ({ parPage, page, searchValue }, { rejectWithValue, fulfillWithValue }) => {
+        try {
+            const { data } = await api.get(`/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`, { withCredentials: true })
+            return fulfillWithValue(data)
+        } catch (error) {
+            return rejectWithValue(error.response.data)
+        }
     }
-  }
-);
+)
+
+
 
 export const categoryReducer = createSlice({
-  name: "category",
-  initialState: {
-    successMessage: "",
-    errorMessage: "",
-    loader: false,
-    categorys: [],
-    totalCategory: 0,
-  },
-  reducers: {
-    messageClear: (state, _) => {
-      state.errorMessage = "";
-      state.successMessage = "";
+    name: 'category',
+    initialState: {
+        successMessage: '',
+        errorMessage: '',
+        loader: false,
+        categorys: [],
+        totalCategory : 0
     },
-  },
-  extraReducers: {
-    [categoryAdd.pending]: (state, _) => {
-      state.loader = true;
+    reducers: {
+        messageClear: (state, _) => {
+            state.errorMessage = ""
+            state.successMessage = ""
+        }
     },
-    [categoryAdd.rejected]: (state, { payload }) => {
-      state.loader = false;
-      state.errorMessage = payload.error;
-    },
-    [categoryAdd.fulfilled]: (state, { payload }) => {
-      state.loader = false;
-      state.successMessage = payload.message;
-      state.categorys = [...state.categorys, payload.category];
-    },
-    [get_category.fulfilled]: (state, { payload }) => {
-      state.totalCategory = payload.totalCategory;
-      state.categorys = payload.categorys;
-    },
-  },
-});
-export const { messageClear } = categoryReducer.actions;
-export default categoryReducer.reducer;
+    extraReducers: {
+        [categoryAdd.pending]: (state, _) => {
+            state.loader = true
+        },
+        [categoryAdd.rejected]: (state, { payload }) => {
+            state.loader = false
+            state.errorMessage = payload.error
+        },
+        [categoryAdd.fulfilled]: (state, { payload }) => {
+            state.loader = false
+            state.successMessage = payload.message
+            state.categorys = [...state.categorys, payload.category]
+        },
+        [get_category.fulfilled]: (state, { payload }) => {
+            state.totalCategory = payload.totalCategory
+            state.categorys = payload.categorys
+        },
+    }
+
+})
+export const { messageClear } = categoryReducer.actions
+export default categoryReducer.reducer
