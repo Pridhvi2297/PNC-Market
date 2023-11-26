b8e8ca9581348d77f6635f1d31897e9e7ec36d2a
prudhvi2297 <mpnc2296@gmail.com>
2023-11-26 07:03:48 -0500

    Frontend Customer Header and banner

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Heders from '../components/Headers'
+import Banner from '../components/Banner'
+
+
+const Home = () => {
+  return (
+    <div className='w-full'>
+        <Heders />
+        <Banner />
+        
+    </div>
+  )
+}
+
+export default Home
