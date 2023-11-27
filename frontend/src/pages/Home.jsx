bb555590e2447310ec4a9437b360794b805cedb6
prudhvi2297 <mpnc2296@gmail.com>
2023-11-27 06:42:47 -0500

    Frontend Customer Footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Banner from "../components/Banner";
 import Categorys from "../components/Categorys";
 import FeatureProducts from "../components/products/FeatureProducts";
 import Products from "../components/products/Products";
+import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
@@ -31,6 +33,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
