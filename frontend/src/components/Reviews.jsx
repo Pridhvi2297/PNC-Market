92587ccc25751fa50e25188e745b9447f23d77fd
prudhvi2297 <mpnc2296@gmail.com>
2023-11-29 05:04:47 -0500

    Frontend Customer ProductDetai-Reviews

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,9 +1,178 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Ratings from "./Ratings";
+import RatingTemp from "./RatingTemp";
+import Pagination from "./Pagination";
+import { CiStar } from "react-icons/ci";
+import { AiFillStar } from "react-icons/ai";
+import RatingReact from "react-rating";
 
 const Reviews = () => {
-  return (
-    <div>Hello Reviews</div>
-  )
-}
+  const userInfo = {};
+  const totalReview = 25;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [rat, setRat] = useState("");
+  const [re, setRe] = useState("");
 
-export default Reviews
+  return (
+    <div className="mt-8">
+      <div className="flex gap-10 md:flex-col">
+        <div className="flex flex-col gap-2 justify-start items-start py-4">
+          <div>
+            <span className="text-6xl font-semibold">4.5</span>
+            <span className="text-3xl font-semibold text-slate-600">/5</span>
+          </div>
+          <div className="flex text-4xl">
+            <Ratings ratings={4.5} />
+          </div>
+          <p className="text-sm text-slate-600">20 Reviews</p>
+        </div>
+        <div className="flex gap-2 flex-col py-4">
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={5} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={4} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={3} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={2} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={1} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-5">
+            <div className="text-md flex gap-1 w-[93px]">
+              <RatingTemp rating={0} />
+            </div>
+            <div className="w-[200px] h-[14px] bg-slate-200 relative">
+              <div className="h-full bg-[#EDBB0E]"></div>
+            </div>
+            <p className="text-sm text-slate-600 w-[0%]">5</p>
+          </div>
+        </div>
+      </div>
+      <h2 className="text-slate-600 text-xl font-bold py-5">
+        Product Reviews {totalReview}
+      </h2>
+      <div className="flex flex-col gap-8 pb-10 pt-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((r, i) => (
+          <div key={i} className="flex flex-col gap-1">
+            <div className="flex justify-between items-center">
+              <div className="flex gap-1 text-xl">
+                <RatingTemp rating={4} />
+              </div>
+              <span className="text-slate-600">22 July 2023</span>
+            </div>
+            <span className="text-slate-600 text-md">Pridhvinath Chowdary</span>
+            <p className="text-slate-600 text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
+              assumenda quibusdam aliquam corporis ea, quos, eum iusto molestiae
+              maxime maiores a debitis ad qui. Sapiente ea at eum cumque illo.
+            </p>
+          </div>
+        ))}
+        <div className="flex justify-end">
+          {totalReview > 5 && (
+            <Pagination
+              pageNumber={pageNumber}
+              setPageNumber={setPageNumber}
+              totalItem={totalReview}
+              perPage={perPage}
+              showItem={Math.round(totalReview / 5)}
+            />
+          )}
+        </div>
+      </div>
+
+      <div>
+        {userInfo ? (
+          <div className="flex flex-col gap-3">
+            <div className="flex gap-1">
+              <RatingReact
+                onChange={(e) => setRat(e)}
+                initialRating={rat}
+                emptySymbol={
+                  <span className="text-slate-600 text-4xl">
+                    <CiStar />
+                  </span>
+                }
+                fullSymbol={
+                  <span className="text-[#EDBB0E] text-4xl">
+                    <AiFillStar />
+                  </span>
+                }
+              />
+            </div>
+            <form>
+              <textarea
+                value={re}
+                required
+                onChange={(e) => setRe(e.target.value)}
+                className="border outline-0 p-3 w-full"
+                name=""
+                id=""
+                cols="30"
+                rows="5"
+              ></textarea>
+              <div className="mt-2">
+                <button className="py-1 px-5 bg-indigo-500 text-white rounded-sm">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div>
+            <Link
+              className="py-1 px-5 bg-indigo-500 text-white rounded-sm"
+              to="/login"
+            >
+              Login
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
