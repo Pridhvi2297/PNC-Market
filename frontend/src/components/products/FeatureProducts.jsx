6d67cce18ee3a0553f915b1eecd54d3f643f0380
prudhvi2297 <mpnc2296@gmail.com>
2023-11-30 08:01:55 -0500

    Backend for Home All Products

--- frontend/src/components/products/FeatureProducts.jsx
+++ frontend/src/components/products/FeatureProducts.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { FcLike, FcPackage, FcBinoculars } from "react-icons/fc";
 import Ratings from "../Ratings";
 
-const FeatureProducts = () => {
+const FeatureProducts = ({ products }) => {
   const add_wishlist = useState("6");
   const add_card = useState("6");
 
@@ -15,14 +15,18 @@ const FeatureProducts = () => {
           <div className="w-[100px] h-[4px] bg-[#7fad39] mt-4"></div>
         </div>
         <div className="w-full grid grid-cols-4 md-lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((p, i) => (
+          {products.map((p, i) => (
             <div className="border group transition-all duration-500 hover:shadow-md hover:-mt-3">
               <div className="relative overflow-hidden">
-                <div className="flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
-                  10%
-                </div>
+                {p.discount ? (
+                  <div className="flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
+                    {p.discount}%
+                  </div>
+                ) : (
+                  ""
+                )}
                 <img
-                  src="/images/banner/8.jpg"
+                  src={p.images[0]}
                   alt=""
                   className="sm:w-full w-full h-[200px]"
                 />
@@ -48,9 +52,9 @@ const FeatureProducts = () => {
                 </ul>
               </div>
               <div className="py-3 text-slate-600 px-2">
-                <h2>Apple</h2>
+                <h2>{p.name}</h2>
                 <div className="flex justify-start items-center gap-3">
-                  <span className="text-lg  font-bold">$1200</span>
+                  <span className="text-lg  font-bold">${p.price}</span>
                   <div className="flex">
                     <Ratings ratings={p.rating} />
                   </div>
@@ -65,4 +69,3 @@ const FeatureProducts = () => {
 };
 
 export default FeatureProducts;
-
--- frontend/src/store/reducers/homeReducer.js
+++ frontend/src/store/reducers/homeReducer.js
@@ -2,11 +2,10 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../api/api";
 
 export const get_category = createAsyncThunk(
-  "category/get_categorys",
+  "product/get_categorys",
   async (_, { fulfillWithValue }) => {
     try {
       const { data } = await api.get("/home/get-categorys");
-      console.log(data);
       return fulfillWithValue(data);
     } catch (error) {
       console.log(error.response);
@@ -14,15 +13,39 @@ export const get_category = createAsyncThunk(
   }
 );
 
+export const get_products = createAsyncThunk(
+  "product/get_products",
+  async (_, { fulfillWithValue }) => {
+    try {
+      const { data } = await api.get("/home/get-products");
+      console.log(data)
+      return fulfillWithValue(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+);
+
+
 export const homeReducer = createSlice({
   name: "home",
   initialState: {
     categorys: [],
+    products: [],
+    latest_product: [],
+    topRated_product: [],
+    discount_product: [],
   },
   reducers: {},
   extraReducers: {
     [get_category.fulfilled]: (state, { payload }) => {
       state.categorys = payload.categorys;
+    },
+    [get_products.fulfilled]: (state, { payload }) => {
+      state.products = payload.products;
+      state.latest_product = payload.latest_product;
+      state.topRated_product = payload.topRated_product;
+      state.discount_product = payload.discount_product;
     },
   },
 });
