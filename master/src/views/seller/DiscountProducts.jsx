59d078f60a8378bce8411ef70c19dc94a18dcb02
prudhvi2297 <mpnc2296@gmail.com>
2023-12-15 04:02:01 -0500

    Fix Seller Discount products view

--- master/src/views/seller/DiscountProducts.jsx
+++ master/src/views/seller/DiscountProducts.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
-import { FaEdit, FaEye, FaTrash } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import Pagination from '../Pagination'
-import Search from '../components/Search'
+import React, { useState, useEffect } from 'react';
+import { FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Pagination from '../Pagination';
+import Search from '../components/Search';
+import { get_products } from '../../store/Reducers/productReducer';
 
 const DiscountProducts = () => {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [searchValue, setSearchValue] = useState('')
-    const [perPage, setPerPage] = useState(5)
+    const dispatch = useDispatch();
+    const { products, totalProduct } = useSelector((state) => state.product);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchValue, setSearchValue] = useState('');
+    const [perPage, setPerPage] = useState(5);
+
+    useEffect(() => {
+        const obj = {
+            perPage: parseInt(perPage),
+            page: parseInt(currentPage),
+            searchValue,
+        };
+        dispatch(get_products(obj));
+    }, [searchValue, currentPage, perPage]);
+
     return (
         <div className='px-2 lg:px-7 pt-5 '>
-            <div className='w-full p-4  bg-black rounded-md'>
+            <div className='w-full p-4 bg-black rounded-md'>
                 <Search setPerPage={setPerPage} setSearchValue={setSearchValue} searchValue={searchValue} />
                 <div className='relative overflow-x-auto mt-5'>
-                    <table className='w-full text-sm text-left text-[#d0d2d6]'>
-                        <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
-                            <tr>
+                    <table className='w-full text-sm text-left text-white'>
+                        <thead className='text-sm text-white uppercase border-b border-slate-700'>
+                        <tr>
                                 <th scope='col' className='py-3 px-4'>No</th>
                                 <th scope='col' className='py-3 px-4'>Image</th>
                                 <th scope='col' className='py-3 px-4'>Name</th>
@@ -28,54 +43,56 @@ const DiscountProducts = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {
-                                [1, 2, 3, 4, 5].map((d, i) => <tr key={i}>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d}</td>
+                            {products
+                                .filter((d) => d.discount > 0) // Filter out products with no discount
+                                .map((d, i) => (
+                                    <tr key={i}>
+                                        <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                                            {i + 1}
+                                        </td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <img className='w-[45px] h-[45px]' src={`https://m.media-amazon.com/images/I/81UKVHM77GL._AC_UY436_FMwebp_QL65_.jpg`} alt="" />
+                                        <img className='w-[45px] h-[45px]' src={d.images[0]} alt="" />
                                     </td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>Men's Premium soft..</span>
+                                        <span>{d?.name?.slice(0, 16)}...</span>
                                     </td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>Sports</span>
+                                        <span>{d.category}</span>
                                     </td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>Easy</span>
+                                        <span>{d.brand}</span>
                                     </td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>$565</span>
+                                        <span>${d.price}</span>
                                     </td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>5%</span>
-                                    </td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <span>10</span>
-                                    </td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
-                                        <div className='flex justify-start items-center gap-4'>
-                                            <Link className='p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50'><FaEdit /></Link>
-                                            <Link className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'><FaEye /></Link>
-                                            <button className='p-[6px] bg-red-500 rounded hover:shadow-lg hover:shadow-red-500/50'><FaTrash /></button>
-                                        </div>
-                                    </td>
-                                </tr>)
-                            }
+                                        <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                                            {d.discount === 0 ? <span>no discount</span> : <span>{d.discount}%</span>}
+                                        </td>
+                                        <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                                            <div className='flex justify-start items-center gap-4'>
+                                                <Link
+                                                    to={`/seller/dashboard/edit-product/${d._id}`}
+                                                    className='p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50'
+                                                >
+                                                    <FaEdit />
+                                                </Link>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
                         </tbody>
                     </table>
                 </div>
-                <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
-                    <Pagination
-                        pageNumber={currentPage}
-                        setPageNumber={setCurrentPage}
-                        totalItem={50}
-                        perPage={perPage}
-                        showItem={4}
-                    />
-                </div>
+                {totalProduct <= perPage ? (
+                    ''
+                ) : (
+                    <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
+                        <Pagination pageNumber={currentPage} setPageNumber={setCurrentPage} totalItem={50} perPage={perPage} showItem={4} />
+                    </div>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default DiscountProducts
+export default DiscountProducts;
